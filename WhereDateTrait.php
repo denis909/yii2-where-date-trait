29664cd9f34ca9b79d882b01a5aeac1d34d86b48
denis909 <denis909@mail.ru>
2020-10-05 10:08:16 +0300

    n/a

--- WhereDateTrait.php
+++ WhereDateTrait.php
@@ -3,20 +3,23 @@
 namespace denis909\yii;
 
 use Exception;
+use DateTime;
 
 trait WhereDateTrait
 {
 
-    public function whereDate($attribute, $value)
+    public function whereDate($attribute, $value, $format = 'Y-m-d')
     {
-        $time = strtotime($value);
+        $date = DateTime::createFromFormat($format, $value);
 
-        if (!$time)
+        if (!$date)
         {
-            throw new Exception('Invalid time format.');
-        }
+            throw new Exception('Invalid date format.');
+        }        
 
-        return $this->andWhere([
+        $time = $date->format('U');
+
+        return $this->where([
             'and',
             ['YEAR(' . $attribute . ')' => date('Y', $time)],
             ['MONTH(' . $attribute . ')' => date('m', $time)],
@@ -29,6 +32,35 @@ trait WhereDateTrait
         if ($value)
         {
             return $this->whereDate($attribute, $value);
+        }
+
+        return $this;
+    }
+
+    public function andWhereDate($attribute, $value, $format = 'Y-m-d')
+    {
+        $date = DateTime::createFromFormat($format, $value);
+
+        if (!$date)
+        {
+            throw new Exception('Invalid date format.');
+        }        
+
+        $time = $date->format('U');
+
+        return $this->andWhere([
+            'and',
+            ['YEAR(' . $attribute . ')' => date('Y', $time)],
+            ['MONTH(' . $attribute . ')' => date('m', $time)],
+            ['DAY(' . $attribute . ')' => date('d', $time)]
+        ]);
+    }
+
+    public function andFilterWhereDate($attribute, $value)
+    {
+        if ($value)
+        {
+            return $this->andWhereDate($attribute, $value);
         }
 
         return $this;
